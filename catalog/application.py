d1e4a2ba0aa09cdffd44cca8eebd776c344dfb97
timothyzhong <timothy7784@gmail.com>
2016-03-02 23:10:23 -0600

    Update directory in application

--- catalog/application.py
+++ catalog/application.py
@@ -138,13 +138,13 @@ def index():
 @app.route('/catalog/<category_name>/items')
 def itemsOfCategory(category_name):
     """Return category page with all its items"""
-    print "looking up items of %s" % category_name
+    # print "looking up items of %s" % category_name
     cat = session.query(Category).filter_by(name=category_name).one()
-    print "cat_id is %s" % cat.id
+    # print "cat_id is %s" % cat.id
     item_list = session.query(Item).filter_by(category_id=cat.id).all()
     category_list = session.query(Category).order_by(Category.name).all()
-    for item in item_list:
-        print "%s of %s" % (item.name, item.category_id)
+    # for item in item_list:
+    #    print "%s of %s" % (item.name, item.category_id)
     return render_template('itemsOfCategory.html', items=item_list,
                            category_name=category_name,
                            category_list=category_list)
@@ -280,7 +280,7 @@ def deleteItem(item_name, category_name):
     if request.method == 'POST':
         try:
             imgdir = os.path.join(app.config['UPLOAD_FOLDER'], item.image)
-            print "delete %s" % imgdir
+            # print "delete %s" % imgdir
             if os.path.isfile(imgdir):
                 os.unlink(imgdir)
         except Exception, e:
@@ -355,6 +355,7 @@ def gconnect():
         return response
 
     # Verify that the access token is valid for this app.
+    print 'issued_to: %s' % result['issued_to']
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
